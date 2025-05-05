d450ff190c2d48d740ba50a5be86460cfc87cfa4
Dean <dean.jobs2021@gmail.com>
2025-05-05 18:31:29 +0800

    fix: update MotionTags with proper types

--- app/MotionTags.tsx
+++ app/MotionTags.tsx
@@ -1,12 +1,12 @@
-'use client';
+'use client'
 
 import { motion } from 'framer-motion';
-import { HTMLMotionProps, motion as fmMotion } from 'framer-motion';
+import type { MotionProps } from 'framer-motion';
 
-export const MotionH1 = (props: HTMLMotionProps<'h1'>) => {
-  return <fmMotion.h1 {...props} />;
+export const MotionH1 = (props: JSX.IntrinsicElements['h1'] & MotionProps) => {
+  return <motion.h1 {...props} />;
 };
 
-export const MotionP = (props: HTMLMotionProps<'p'>) => {
-  return <fmMotion.p {...props} />;
+export const MotionP = (props: JSX.IntrinsicElements['p'] & MotionProps) => {
+  return <motion.p {...props} />;
 };
