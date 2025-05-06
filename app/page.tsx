44949376899c9c6059e341bf31c3861e9b042783
Dean <dean.jobs2021@gmail.com>
2025-05-06 10:36:42 +0800

    page 導入Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import HeroSection from './HeroSection';
-import AboutSection from './AboutSection';
-import WorkSection from './WorkSection';
-import ContactSection from './ContactSection';
-import Footer from './Footer';
+// app/page.tsx
+import HeroSection from './HeroSection'
+import AboutSection from './AboutSection'
+import WorkSection from './WorkSection'
+import ContactSection from './ContactSection'
+import Footer from './Footer'
 
-export default function Portfolio() {
+export default function Page() {
   return (
-    <main className="min-h-screen w-full bg-white text-black font-sans p-8 space-y-20">
+    <main>
       <HeroSection />
       <AboutSection />
       <WorkSection />
       <ContactSection />
       <Footer />
     </main>
-  );
+  )
 }
