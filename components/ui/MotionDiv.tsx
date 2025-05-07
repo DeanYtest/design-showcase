2faceab5de472c2a9a67d0e76092e30a3f54c5f6
Dean <dean.jobs2021@gmail.com>
2025-05-07 16:22:28 +0800

    fix:MotionDiv-hover13

--- components/ui/MotionDiv.tsx
+++ components/ui/MotionDiv.tsx
@@ -1,5 +1,9 @@
 'use client'
 import { motion } from 'framer-motion'
+import type { HTMLMotionProps } from 'framer-motion'
 
-const MotionDiv = motion.div
+const MotionDiv = (props: HTMLMotionProps<'div'>) => {
+  return <motion.div {...props} />
+}
+
 export default MotionDiv
