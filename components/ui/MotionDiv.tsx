5bd8ebbfe3388ca716834c9261e8a36bfaf082f8
Dean <dean.jobs2021@gmail.com>
2025-05-07 18:12:08 +0800

    fix:TiltCard21

--- components/ui/MotionDiv.tsx
+++ components/ui/MotionDiv.tsx
@@ -4,8 +4,12 @@ import { motion } from 'framer-motion'
 import type { HTMLMotionProps } from 'framer-motion'
 import { forwardRef } from 'react'
 
-const MotionDiv = forwardRef<HTMLDivElement, HTMLMotionProps<'div'>>(function MotionDiv(props, ref) {
-  return <motion.div ref={ref} {...props} />
-})
+const MotionDiv = forwardRef<HTMLDivElement, HTMLMotionProps<'div'>>(
+  (props, ref) => {
+    return <motion.div ref={ref} {...props} />
+  }
+)
+
+MotionDiv.displayName = 'MotionDiv'
 
 export default MotionDiv
