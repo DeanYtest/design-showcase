fbe9f8d7a038b2a01870e3053e07eb149690b8d5
Dean <dean.jobs2021@gmail.com>
2025-05-07 16:27:51 +0800

    fix:MotionDiv-hover16

--- components/ui/MotionDiv.tsx
+++ components/ui/MotionDiv.tsx
@@ -2,9 +2,14 @@
 
 import { motion } from 'framer-motion'
 import type { HTMLMotionProps } from 'framer-motion'
+import type { ForwardedRef } from 'react'
+import { forwardRef } from 'react'
 
-const MotionDiv: React.FC<HTMLMotionProps<'div'>> = (props) => {
-  return <motion.div {...props} />
-}
+const MotionDiv = forwardRef(function MotionDiv(
+  props: HTMLMotionProps<'div'>,
+  ref: ForwardedRef<HTMLDivElement>
+) {
+  return <motion.div ref={ref} {...props} />
+})
 
 export default MotionDiv
