8d64e6b7c4cadb443c557771659e5dd0b4a595be
Dean <dean.jobs2021@gmail.com>
2025-05-07 17:21:03 +0800

    fix:TiltCard8

--- components/ui/TiltCard.tsx
+++ components/ui/TiltCard.tsx
@@ -2,35 +2,35 @@
 
 import { useRef } from 'react'
 import { useMotionValue, useTransform, motion } from 'framer-motion'
+import type { ReactNode, MouseEvent } from 'react'
 import clsx from 'clsx'
-import type { ReactNode } from 'react'
 
 interface TiltCardProps {
   children: ReactNode
   className?: string
 }
 
-const TiltCard = ({ children, className }: TiltCardProps) => {
+export default function TiltCard({ children, className }: TiltCardProps) {
   const ref = useRef<HTMLDivElement>(null)
   const x = useMotionValue(0)
   const y = useMotionValue(0)
   const rotateX = useTransform(y, [-50, 50], [15, -15])
   const rotateY = useTransform(x, [-50, 50], [-15, 15])
 
-  const handleMouse = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  function handleMouse(e: MouseEvent<HTMLDivElement>) {
     const rect = ref.current?.getBoundingClientRect()
     if (!rect) return
     x.set(e.clientX - rect.left - rect.width / 2)
     y.set(e.clientY - rect.top - rect.height / 2)
   }
 
-  const handleMouseLeave = () => {
+  function handleMouseLeave() {
     x.set(0)
     y.set(0)
   }
 
   return (
-    <motion.div
+    <motion.div<HTMLDivElement>
       ref={ref}
       onMouseMove={handleMouse}
       onMouseLeave={handleMouseLeave}
@@ -45,5 +45,3 @@ const TiltCard = ({ children, className }: TiltCardProps) => {
     </motion.div>
   )
 }
-
-export default TiltCard
