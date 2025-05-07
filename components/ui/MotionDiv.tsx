e8afd8fe53ab8acc97e838d9549735de746c033f
Dean <dean.jobs2021@gmail.com>
2025-05-07 16:01:52 +0800

    fix:MotionDiv-hover1

--- components/ui/MotionDiv.tsx
+++ components/ui/MotionDiv.tsx
@@ -1,29 +1,14 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import {
-  HTMLMotionProps,
-  MotionStyle,
-  MotionValue,
-} from 'framer-motion'
-import {
-  HTMLAttributes,
-  forwardRef,
-  ForwardedRef,
-} from 'react'
+import { forwardRef } from 'react'
+import type { HTMLMotionProps } from 'framer-motion'
 
-// 這裡你只要用 framer-motion 提供的 HTMLMotionProps 即可，不需自己組合
 type Props = HTMLMotionProps<'div'>
 
-const MotionDiv = forwardRef<HTMLDivElement, Props>(
-  ({ children, ...rest }: Props, ref: ForwardedRef<HTMLDivElement>) => {
-    return (
-      <motion.div ref={ref} {...rest}>
-        {children}
-      </motion.div>
-    )
-  }
-)
+const MotionDiv = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  return <motion.div ref={ref} {...props} />
+})
 
 MotionDiv.displayName = 'MotionDiv'
 
