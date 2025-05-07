c983153c4842ef4ca45f769a888f4f1bb9901f20
Dean <dean.jobs2021@gmail.com>
2025-05-07 16:36:31 +0800

    fix:MotionDiv-hover19

--- components/ui/MotionDiv.tsx
+++ components/ui/MotionDiv.tsx
@@ -5,13 +5,11 @@ import type { HTMLMotionProps } from 'framer-motion'
 import { forwardRef } from 'react'
 
 const MotionDiv = forwardRef<HTMLDivElement, HTMLMotionProps<'div'>>(
-  ({ children, ...props }, ref) => {
-    return (
-      <motion.div ref={ref} {...props}>
-        {children}
-      </motion.div>
-    )
-  }
+  ({ children, ...props }, ref) => (
+    <motion.div ref={ref} {...props}>
+      {children}
+    </motion.div>
+  )
 )
 
 MotionDiv.displayName = 'MotionDiv'
