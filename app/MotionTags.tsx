8404673c320ab05190a827b1df2ff4806dc034e5
Dean <dean.jobs2021@gmail.com>
2025-05-14 11:59:23 +0800

    fix:MotionTags

--- app/MotionTags.tsx
+++ app/MotionTags.tsx
@@ -1,8 +1,21 @@
+// app/MotionTags.tsx
 'use client';
-import { motion, HTMLMotionProps } from 'framer-motion';
-import React from 'react';
 
-// 讓這些元件既承襲 MotionProps，也能正確接收 className、style…等 HTML 屬性
-export const MotionDiv: React.FC<HTMLMotionProps<'div'>> = (props) => <motion.div {...props} />;
-export const MotionH1: React.FC<HTMLMotionProps<'h1'>> = (props) => <motion.h1 {...props} />;
-export const MotionP:  React.FC<HTMLMotionProps<'p'>>  = (props) => <motion.p  {...props} />;
+import React from 'react';
+import {
+  motion as _motion,
+  AnimatePresence as _AnimatePresence,
+  HTMLMotionProps,
+  MotionProps,
+} from 'framer-motion';
+
+// 重新導出 motion 及 AnimatePresence
+export const motion = _motion;
+export const AnimatePresence = _AnimatePresence;
+
+// 通用 Motion 元件，支援 className/style 等 HTML 屬性
+export const MotionDiv: React.FC<HTMLMotionProps<'div'>> = (props) => <_motion.div {...props} />;
+export const MotionH1: React.FC<HTMLMotionProps<'h1'>> = (props) => <_motion.h1 {...props} />;
+export const MotionP: React.FC<HTMLMotionProps<'p'>> = (props) => <_motion.p {...props} />;
+export const MotionSpan: React.FC<HTMLMotionProps<'span'>> = (props) => <_motion.span {...props} />;
+export const MotionButton: React.FC<HTMLMotionProps<'button'>> = (props) => <_motion.button {...props} />;
