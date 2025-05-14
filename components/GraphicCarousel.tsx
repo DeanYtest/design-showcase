73f3df0038cf2e016d4d555f5763e85ea4c587d5
Dean <dean.jobs2021@gmail.com>
2025-05-14 13:32:19 +0800

    fix:graphic animation11

--- components/GraphicCarousel.tsx
+++ components/GraphicCarousel.tsx
@@ -8,7 +8,6 @@ interface GraphicCarouselProps {
   onSelect: (img: string) => void;
 }
 
-// 重複示例圖，共 20 張
 const images: string[] = Array.from(
   { length: 20 },
   (_, i) => `/images/graphic/image${(i % 5) + 1}.jpg`
@@ -17,29 +16,32 @@ const images: string[] = Array.from(
 export default function GraphicCarousel({ onSelect }: GraphicCarouselProps) {
   return (
     <div className="relative w-full h-[600px] md:h-screen flex items-center justify-center overflow-hidden">
-      {/* 旋轉容器：使用 MotionDiv，支持 className */}
-      <MotionDiv
-        className="absolute left-1/2 top-1/2 w-[80vmin] h-[80vmin] -translate-x-1/2 -translate-y-1/2"
-        animate={{ rotate: 360 }}
-        transition={{ repeat: Infinity, duration: 48, ease: 'linear' }}
-        style={{ transformOrigin: 'center center' }}
-      >
-        {images.map((img, i) => {
-          const angle = (360 / images.length) * i;
-          return (
-            <div
-              key={i}
-              className="absolute left-1/2 top-1/2 origin-center cursor-pointer"
-              style={{ transform: `rotate(${angle}deg) translate(40vmin) rotate(${-angle}deg)` }}
-              onClick={() => onSelect(img)}
-            >
-              <GraphicItem src={img} hoverEffect="flip" />
-            </div>
-          );
-        })}
-      </MotionDiv>
+      {/* 定位容器：控制圓環位置及大小 */}
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[80vmin] h-[80vmin]">
+        {/* 旋轉容器：只控制旋轉，不影響定位 */}
+        <MotionDiv
+          className="w-full h-full origin-center"
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 48, ease: 'linear' }}
+          style={{ transformOrigin: 'center center' }}
+        >
+          {images.map((img, i) => {
+            const angle = (360 / images.length) * i;
+            return (
+              <div
+                key={i}
+                className="absolute left-1/2 top-1/2 origin-center cursor-pointer"
+                style={{ transform: `rotate(${angle}deg) translate(40vmin) rotate(${-angle}deg)` }}
+                onClick={() => onSelect(img)}
+              >
+                <GraphicItem src={img} hoverEffect="flip" />
+              </div>
+            );
+          })}
+        </MotionDiv>
+      </div>
 
-      {/* 靜態中心文本與提示 */}
+      {/* 靜態中心文字及提示 */}
       <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
         <h2 className="text-3xl font-semibold">平面設計</h2>
         <p className="mt-2 text-sm text-gray-500">click to explore</p>
